2066273e9b0151b6982e6887afd472a1ecaff458
Sarah <sarahehi.sa@gmail.com>
2021-10-04 07:54:36 +0100

    Added styles to dashboard

--- src/home/Index.jsx
+++ src/home/Index.jsx
@@ -9,7 +9,7 @@ function Home() {
             <div className="dashboard">
                 <div className="row px-1">
                     <div className="col-8">
-                        <h4>Dashboard</h4>
+                        <h4 className="font-weight-bold dash">Dashboard</h4>
                     </div>
                     <div className="col-4">
                         <div className="float-right">
@@ -21,42 +21,48 @@ function Home() {
                 <div className="row px-1">
                     <div className="col-7 d-none d-md-block dash-overview">
                         <div className="fixed mb-5">
-                            <h5>Overview</h5>
-                            <div className="row d-flex justify-content-around">
-                                <div className="col-6 round-img total mx-3">
-                                    <div className="">
-                                        <h4 className="d-flex justify-content-center">12,000</h4>
-                                        <h6 className="d-flex justify-content-center">Total Points</h6>
+                            <h5 className="font-weight-bold dash-sections">Overview</h5>
+                            <div className="row d-flex justify-content-between">
+                                <div className="col-6 round-img overview-points total mx-3">
+                                    <div className="pt-5">
+                                        <h3 className="d-flex justify-content-center">12,000</h3>
+                                        <h6 className="d-flex point-bal justify-content-center">Total Point</h6>
                                     </div>
                                 </div>
-                                <div className="col-6 round-img total mx-3">
-                                    <div className="">
-                                        <h4 className="d-flex justify-content-center">20,000</h4>
-                                        <h6 className="d-flex justify-content-center">Total Balance</h6>
+                                <div className="col-6 round-img overview-bal total mx-3">
+                                    <div className="pt-5">
+                                        <h3 className="d-flex justify-content-center">N20,000</h3>
+                                        <h6 className="d-flex point-bal justify-content-center">Total Balance</h6>
                                     </div>
                                 </div>
                             </div>
                         </div>
                         <div className="row my-5">
                             <div className="col-6">
-                                <h5>Quick Links</h5>
+                                <h5 className="font-weight-bold dash-sections">Quick Links</h5>
                                 <div className="row">
                                     <div className="col-4">
-                                        <img src="" alt="" />
-                                        Transfer
+                                        <div className="ql-icon">
+                                            <img src="/src/images/Send(arrow).png" alt="" className="pt-2"/>
+                                        </div>
+                                        <h6>Transfer</h6>
                                     </div>
                                     <div className="col-4">
-                                        <img src="" alt="" />
-                                        Airtime
+                                        <div className="ql-icon">
+                                            <img src="/src/images/Layer 2.svg" alt="" className=" pt-3" />
+                                        </div>
+                                        <h6>Airtime</h6>
                                     </div>
                                     <div className="col-4">
-                                        <img src="" alt="" />
-                                        Data
+                                        <div className="ql-icon">
+                                            <img src="/src/images/Send(arrow).png" alt="" className=" pt-2" />
+                                        </div>
+                                        <h6>Data</h6>
                                     </div>
                                 </div>
                             </div>
                             <div className="col-6">
-                                <h5>Free Transfer</h5>
+                                <h5 className="font-weight-bold dash-sections">Free Transfer</h5>
                                 <div className="row">
                                     <div className="col-4">
                                         <img src="" alt="" />
@@ -74,7 +80,7 @@ function Home() {
                             </div>
                         </div>
                         <div className="my-5">
-                            <h5>Get the most out of your money</h5>
+                            <h5 className="font-weight-bold dash-sections">Get the most out of your money</h5>
                             <div>
                                 <div className="d-flex">
                                     <img src="" alt="" />
@@ -83,7 +89,7 @@ function Home() {
                             </div>
                         </div>
                         <div className="my-5">
-                            <h5>Refer and earn</h5>
+                            <h5 className="font-weight-bold dash-sections">Refer and earn</h5>
                             <img src="" alt="megaphone" />
                         </div>
                     </div>
@@ -91,7 +97,7 @@ function Home() {
                         <div className="fixed mb-3">
                             <h5>Overview</h5>
                             <div className="row d-flex justify-content-around">
-                                <div className="col-6 round-img total m-3">
+                                <div className="col-6 round-img  total m-3">
                                     <div className="overview">
                                         <h4 className="d-flex justify-content-center">12,000</h4>
                                         <h6 className="d-flex justify-content-center">Total Points</h6>
